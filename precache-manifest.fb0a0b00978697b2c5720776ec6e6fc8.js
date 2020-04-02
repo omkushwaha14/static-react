85880deda878cd083b2cf7524a9f19b181af3cbc
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-04-02 12:37:03 +0545

    Updates

---
+++ precache-manifest.fb0a0b00978697b2c5720776ec6e6fc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d09eac35c228c8071ff6633ed7d593",
+    "url": "/work-app/index.html"
+  },
+  {
+    "revision": "e88724fde42d2e87d3d5",
+    "url": "/work-app/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "2413a91a00eb2fc27ea6",
+    "url": "/work-app/static/css/main.6fb8e0b8.chunk.css"
+  },
+  {
+    "revision": "e88724fde42d2e87d3d5",
+    "url": "/work-app/static/js/2.dcba95d9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/work-app/static/js/2.dcba95d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2413a91a00eb2fc27ea6",
+    "url": "/work-app/static/js/main.5dc60cf8.chunk.js"
+  },
+  {
+    "revision": "4f45f74817f26fcd3d70",
+    "url": "/work-app/static/js/runtime-main.2d114a09.js"
+  }
+]);
